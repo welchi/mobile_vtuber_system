2017bf31cb12c97c25c0bc6ea11e092e5934ee86
welchi <welchizm@gmail.com>
2019-12-11 18:04:49 +0900

    Add DlibWebCamFaceDetector and FrontalFaceParam

---
+++ Assets/DlibWebCamFaceDetector.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DlibFaceLandmarkDetector;
+using DlibFaceLandmarkDetector.UnityUtils;
+
+public class DlibWebCamFaceDetector : MonoBehaviour
+{
+    // トラッキング状況を表示するRawImage
+    public GameObject surface;
+
+    // トラッキング関連
+    [SerializeField] int requestedWidth = 320;
+    [SerializeField] int requestedHeight = 240;
+    [SerializeField] int requestedFps = 30;
+    [SerializeField] bool requestedIsFrontFacing = true;
+
+    // WebCamTexture関連
+    private WebCamTexture _webCamTexture;
+    private WebCamDevice _webCamDevice;
+    private Color32[] _colors;
+    private Color32[] _rotatedColors;
+    private bool _rotate90Degree = false;
+    private bool _isInitWaiting = false;
+    private bool _hasInitDone = false;
+    private ScreenOrientation _screenOrientation;
+    private int _screenWidth;
+    private int _screenHeight;
+
+    // Landmark関連
+    private FaceLandmarkDetector faceLandmarkDetector;
+    private Texture2D _texture;
+    private string _dlibShapePredictorFileName = "sp_human_face_68.dat";
+    private string _dlibShapePredictorFilePath;
+    private List<Vector2> _landmarks;
+
+    public List<Vector2> Landmarks
+    {
+        get { return _landmarks; }
+        set { _landmarks = value; }
+    }
+
+    void Start()
+    {
+        _dlibShapePredictorFilePath = Utils.getFilePath(_dlibShapePredictorFileName);
+        Run();
+    }
+
+    private void Run()
+    {
+        if (string.IsNullOrEmpty(_dlibShapePredictorFilePath))
+            Debug.LogError(
+                "shape predictor ファイルが見つかりません。 “DlibFaceLandmarkDetector/StreamingAssets/” を “Assets/StreamingAssets/” へ置いてください。");
+
+        faceLandmarkDetector = new FaceLandmarkDetector(_dlibShapePredictorFilePath);
+        Initialize();
+    }
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Initialize()
+    {
+        if (_isInitWaiting)
+            return;
+        StartCoroutine(_Initialize());
+    }
+
+    private IEnumerator _Initialize()
+    {
+        if (_hasInitDone)
+            Dispose();
+
+        _isInitWaiting = true;
+
+        // フロントカメラを取得
+        if (_webCamTexture == null)
+        {
+            for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++)
+            {
+                if (WebCamTexture.devices[cameraIndex].isFrontFacing == requestedIsFrontFacing)
+                {
+                    _webCamDevice = WebCamTexture.devices[cameraIndex];
+                    _webCamTexture =
+                        new WebCamTexture(_webCamDevice.name, requestedWidth, requestedHeight, requestedFps);
+                    break;
+                }
+            }
+        }
+
+        if (_webCamTexture == null)
+        {
+            Debug.LogError("デバイスにフロントカメラが存在しませんでした。");
+            _isInitWaiting = false;
+            yield break;
+        }
+
+        // カメラ再生
+        _webCamTexture.Play();
+        while (true)
+        {
+            // 現フレームのバッファが更新されるまで待つ
+            if (_webCamTexture.didUpdateThisFrame)
+            {
+                _screenOrientation = Screen.orientation;
+                _screenWidth = Screen.width;
+                _screenHeight = Screen.height;
+                _isInitWaiting = false;
+                _hasInitDone = true;
+
+                OnInited();
+                break;
+            }
+            else
+            {
+                yield return 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// WebCamTexture初期化
+    /// </summary>
+    private void OnInited()
+    {
+        // 色を初期化
+        if (_colors == null || _colors.Length != _webCamTexture.width * _webCamTexture.height)
+        {
+            _colors = new Color32[_webCamTexture.width * _webCamTexture.height];
+            _rotatedColors = new Color32[_webCamTexture.width * _webCamTexture.height];
+        }
+
+        // トラッキング画像を画面の向きに合わせる
+        if (Screen.orientation == ScreenOrientation.Portrait ||
+            Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+            _rotate90Degree = true;
+        else
+            _rotate90Degree = false;
+        if (_rotate90Degree)
+            _texture = new Texture2D(_webCamTexture.height, _webCamTexture.width, TextureFormat.RGBA32, false);
+        else
+            _texture = new Texture2D(_webCamTexture.width, _webCamTexture.height, TextureFormat.RGBA32, false);
+
+        surface.GetComponent<RawImage>().texture = _texture;
+        surface.GetComponent<RectTransform>().sizeDelta = new Vector2(320, 240);
+    }
+
+    void Update()
+    {
+        // 画面の回転を取得
+        // 傾きが変わった場合
+        if (_screenOrientation != Screen.orientation &&
+            (_screenWidth != Screen.width || _screenHeight != Screen.height))
+        {
+            Initialize();
+        }
+        else
+        {
+            _screenWidth = Screen.width;
+            _screenHeight = Screen.height;
+        }
+
+        // ランドマーク推定
+        if (_hasInitDone && _webCamTexture.isPlaying && _webCamTexture.didUpdateThisFrame)
+        {
+            Color32[] colors = GetColors();
+            if (colors != null)
+            {
+                faceLandmarkDetector.SetImage<Color32>(colors, _texture.width, _texture.height, 4, true);
+
+                // 顔が含まれる領域を取得
+                List<Rect> detectResult = faceLandmarkDetector.Detect();
+
+                foreach (var rect in detectResult)
+                {
+                    // 顔認識(ランドマーク推定)
+                    Landmarks = faceLandmarkDetector.DetectLandmark(rect);
+                    // ランドマークを描画
+                    faceLandmarkDetector.DrawDetectLandmarkResult<Color32>(colors, _texture.width, _texture.height, 4,
+                        true, 0, 255, 0, 255);
+                }
+
+                // 顔領域を矩形で描画
+                faceLandmarkDetector.DrawDetectResult<Color32>(colors, _texture.width, _texture.height, 4, true, 255, 0,
+                    0, 255, 2);
+
+                _texture.SetPixels32(colors);
+                _texture.Apply(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// WebCamTextureが映した画像をRGBAとして取得
+    /// </summary>
+    private Color32[] GetColors()
+    {
+        _webCamTexture.GetPixels32(_colors);
+
+
+        if (_rotate90Degree)
+        {
+            Rotate90CW(_colors, _rotatedColors, _webCamTexture.width, _webCamTexture.height);
+            FlipColors(_rotatedColors, _webCamTexture.width, _webCamTexture.height);
+            return _rotatedColors;
+        }
+        else
+        {
+            FlipColors(_colors, _webCamTexture.width, _webCamTexture.height);
+            return _colors;
+        }
+
+        return _colors;
+    }
+
+
+    /// <summary>
+    /// 映像を反転
+    /// </summary>
+    void FlipColors(Color32[] colors, int width, int height)
+    {
+        int flipCode = int.MinValue;
+
+        if (_webCamDevice.isFrontFacing)
+        {
+            if (_webCamTexture.videoRotationAngle == 0)
+                flipCode = 1;
+            else if (_webCamTexture.videoRotationAngle == 90)
+                flipCode = 1;
+            else if (_webCamTexture.videoRotationAngle == 180)
+                flipCode = 0;
+            else if (_webCamTexture.videoRotationAngle == 270)
+                flipCode = 0;
+        }
+
+        // 反転が必要な場合
+        if (flipCode > int.MinValue)
+        {
+            if (_rotate90Degree)
+            {
+                if (flipCode == 0)
+                    FlipVertical(colors, colors, height, width);
+                else if (flipCode == 1)
+                    FlipHorizontal(colors, colors, height, width);
+                else if (flipCode < 0)
+                    Rotate180(colors, colors, height, width);
+            }
+            else
+            {
+                if (flipCode == 0)
+                    FlipVertical(colors, colors, width, height);
+                else if (flipCode == 1)
+                    FlipHorizontal(colors, colors, width, height);
+                else if (flipCode < 0)
+                    Rotate180(colors, colors, height, width);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 映像を反転(縦)
+    /// </summary>
+    void FlipVertical(Color32[] src, Color32[] dst, int width, int height)
+    {
+        for (var i = 0; i < height / 2; i++)
+        {
+            var y = i * width;
+            var x = (height - i - 1) * width;
+            for (var j = 0; j < width; j++)
+            {
+                int s = y + j;
+                int t = x + j;
+                Color32 c = src[s];
+                dst[s] = src[t];
+                dst[t] = c;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 映像を反転(横)
+    /// </summary>
+    void FlipHorizontal(Color32[] src, Color32[] dst, int width, int height)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            int y = i * width;
+            int x = y + width - 1;
+            for (var j = 0; j < width / 2; j++)
+            {
+                int s = y + j;
+                int t = x - j;
+                Color32 c = src[s];
+                dst[s] = src[t];
+                dst[t] = c;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 映像を180度回転
+    /// </summary>
+    void Rotate180(Color32[] src, Color32[] dst, int height, int width)
+    {
+        int i = src.Length;
+        for (int x = 0; x < i / 2; x++)
+        {
+            Color32 t = src[x];
+            dst[x] = src[i - x - 1];
+            dst[i - x - 1] = t;
+        }
+    }
+
+    /// <summary>
+    /// 映像を時計回りに90度回転
+    /// </summary>
+    void Rotate90CW(Color32[] src, Color32[] dst, int height, int width)
+    {
+        int i = 0;
+        for (int x = height - 1; x >= 0; x--)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                dst[i] = src[x + y * height];
+                i++;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Destroyイベントの処理
+    /// </summary>
+    private void OnDestroy()
+    {
+        Dispose();
+        if (faceLandmarkDetector != null)
+            faceLandmarkDetector.Dispose();
+    }
+
+    /// <summary>
+    /// すべてのリソースを解放
+    /// </summary>
+    private void Dispose()
+    {
+        _rotate90Degree = false;
+        _isInitWaiting = false;
+        _hasInitDone = false;
+        if (_webCamTexture != null)
+        {
+            _webCamTexture.Stop();
+            Destroy(_webCamTexture);
+            _webCamTexture = null;
+        }
+
+        if (_texture != null)
+        {
+            Destroy(_texture);
+            _texture = null;
+        }
+    }
+}
---
+++ Assets/FrontalFaceParam.cs
@@ -0,0 +1,256 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Live2D.Cubism.Core;
+using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.Calib3dModule;
+using OpenCVForUnity.UnityUtils;
+using static DlibWebCamFaceDetector;
+
+public class FrontalFaceParam : MonoBehaviour
+{
+    [SerializeField] DlibWebCamFaceDetector faceDetector;
+    [SerializeField] CubismParameter headAngleParameterX;
+    [SerializeField] CubismParameter headAngleParameterY;
+    [SerializeField] CubismParameter headAngleParameterZ;
+    [SerializeField] float lerpT = 0.2f;
+    private Vector3 _headRotation;
+
+    // 座標変換関連
+    private MatOfPoint3f _objectPoints;
+    private MatOfPoint2f _imagePoints;
+    private Mat _rotM;
+    private Mat _camMatrix;
+    private MatOfDouble _distCoeffs;
+    private Matrix4x4 _invertYM;
+    private Matrix4x4 _invertZM;
+    private Matrix4x4 _transformationM;
+    private Mat _rVec;
+    private Mat _tVec;
+    private Matrix4x4 _ARM;
+    private PoseData _oldPoseData;
+    private Matrix4x4 _VP;
+
+    // 正規化関連
+    [SerializeField] float normWidth = 200;
+    [SerializeField] float normHeight = 200;
+    List<Vector2> _normPoints;
+
+    // ローパスフィルタ関連
+    [SerializeField] float positionLowPass = 4f;
+    [SerializeField] float rotationLowPass = 2f;
+
+    void Awake()
+    {
+        _invertYM = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, -1, 1));
+        _invertZM = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, -1));
+
+        // 顔の初期位置を設定
+        _objectPoints = new MatOfPoint3f(
+            new Point3(-31, 72, 86), // 左目
+            new Point3(31, 72, 86), // 右目
+            new Point3(0, 40, 114), // 鼻
+            new Point3(-20, 15, 90), // 左口角
+            new Point3(20, 15, 90), // 右口角
+            new Point3(-69, 76, -2), // 左耳
+            new Point3(69, 76, -2) // 右耳
+        );
+        _imagePoints = new MatOfPoint2f();
+        _rotM = new Mat(3, 3, CvType.CV_64FC1);
+
+        // カメラの内部パラメータ
+        float maxD = Mathf.Max(normHeight, normWidth);
+        float fx = maxD;
+        float fy = maxD;
+        float cx = normWidth / 2.0f;
+        float cy = normHeight / 2.0f;
+        _camMatrix = new Mat(3, 3, CvType.CV_64FC1);
+        _camMatrix.put(0, 0, fx);
+        _camMatrix.put(0, 1, 0);
+        _camMatrix.put(0, 2, cx);
+        _camMatrix.put(1, 0, 0);
+        _camMatrix.put(1, 1, fy);
+        _camMatrix.put(1, 2, cy);
+        _camMatrix.put(2, 0, 0);
+        _camMatrix.put(2, 1, 0);
+        _camMatrix.put(2, 2, 1.0f);
+
+        _distCoeffs = new MatOfDouble(0, 0, 0, 0);
+
+
+        // カメラキャリブレーション
+        Matrix4x4 P = ARUtils.CalculateProjectionMatrixFromCameraMatrixValues((float) fx, (float) fy, (float) cx,
+            (float) cy, normWidth, normHeight, 0.3f, 2000f);
+        Matrix4x4 V = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, -1));
+        _VP = P * V;
+
+        _normPoints = new List<Vector2>(68);
+        for (int i = 0; i < 68; i++)
+            _normPoints.Add(new Vector2(0, 0));
+    }
+
+    private void LateUpdate()
+    {
+        var landmarks = faceDetector.Landmarks;
+        Vector3 angles = GetFrontalFaceAngle(landmarks);
+        // 検出ミスしたら補正をかける
+        float angleX = (angles.x > 180) ? angles.x - 360 : angles.x;
+        float angleY = (angles.y > 180) ? angles.y - 360 : angles.y;
+        float angleZ = (angles.z > 180) ? angles.z - 360 : angles.z;
+        angleX = (angles.x < -180) ? angles.x + 360 : angles.x;
+        angleY = (angles.y < -180) ? angles.y + 360 : angles.y;
+        angleZ = (angles.z < -180) ? angles.z + 360 : angles.z;
+        // 座標系を変換
+        float temp = angleX;
+        angleX = -angleY;
+        angleY = temp;
+        angleZ = -angleZ;
+        
+        // 線形補間によるスムージング
+        _headRotation=new Vector3(
+            Mathf.LerpAngle(_headRotation.x,angleX,lerpT),
+            Mathf.LerpAngle(_headRotation.y,angleY,lerpT),
+            Mathf.LerpAngle(_headRotation.z,angleZ,lerpT));
+        
+        SetParameter(headAngleParameterX,_headRotation.x);
+        SetParameter(headAngleParameterY,_headRotation.y);
+        SetParameter(headAngleParameterZ,_headRotation.z);
+        
+    }
+
+    /// <summary>
+    /// 顔の向きを取得
+    /// </summary>
+    private Vector3 GetFrontalFaceAngle(List<Vector2> points)
+    {
+        if (points.Count != 68)
+            throw new ArgumentNullException("ランドマークが正しくありません。");
+
+        if (_camMatrix == null)
+            throw new ArgumentNullException("カメラの内部パラメータが正しくありません。");
+
+        // スケールの正規化
+        float normScale = Math.Abs(points[30].y - points[8].y) / (normHeight / 2);
+        Vector2 normDiff = points[30] * normScale - new Vector2(normWidth / 2, normHeight / 2);
+
+        for (int i = 0; i < points.Count; i++)
+            _normPoints[i] = points[i] * normScale - normDiff;
+
+        _imagePoints.fromArray(
+            new Point((_normPoints[38].x + _normPoints[41].x) / 2, (_normPoints[38].y + _normPoints[41].y) / 2), // 左目
+            new Point((_normPoints[43].x + _normPoints[46].x) / 2, (_normPoints[43].y + _normPoints[46].y) / 2), // 右目
+            new Point(_normPoints[33].x, _normPoints[33].y), // 鼻
+            new Point(_normPoints[48].x, _normPoints[48].y), // 左口角
+            new Point(_normPoints[54].x, _normPoints[54].y), // 右口角
+            new Point(_normPoints[0].x, _normPoints[0].y), // 左耳
+            new Point(_normPoints[16].x, _normPoints[16].y) // 右耳
+        );
+
+        // 2-3次元対応点から頭部を姿勢推定する
+        if (_rVec == null || _tVec == null)
+        {
+            _rVec = new Mat(3, 1, CvType.CV_64FC1);
+            _tVec = new Mat(3, 1, CvType.CV_64FC1);
+            Calib3d.solvePnP(_objectPoints, _imagePoints, _camMatrix, _distCoeffs, _rVec, _tVec);
+        }
+
+        double tVecX = _tVec.get(0, 0)[0];
+        double tVecY = _tVec.get(1, 0)[0];
+        double tVecZ = _tVec.get(2, 0)[0];
+        bool isNotInViewport = false;
+        Vector4 pos = _VP * new Vector4((float) tVecX, (float) tVecY, (float) tVecZ, 1.0f);
+        if (pos.w != 0)
+        {
+            float x = pos.x / pos.w, y = pos.y / pos.w, z = pos.z / pos.w;
+            if (x < -1.0f || x > 1.0f || y < -1.0f || y > 1.0f || z < -1.0f || z > 1.0f)
+                isNotInViewport = true;
+        }
+
+        // オブジェクトがカメラ視野に存在しない場合、外部パラメータを使用しない
+        if (double.IsNaN(tVecZ) || isNotInViewport)
+        {
+            Calib3d.solvePnP(_objectPoints, _imagePoints, _camMatrix, _distCoeffs,
+                _rVec, _tVec);
+        }
+        else
+        {
+            Calib3d.solvePnP(_objectPoints, _imagePoints, _camMatrix, _distCoeffs,
+                _rVec, _tVec, true, Calib3d.SOLVEPNP_ITERATIVE);
+        }
+
+        if (!isNotInViewport)
+        {
+            // Unityのポーズデータに変換
+            double[] rVecArr = new double[3];
+            _rVec.get(0, 0, rVecArr);
+            double[] tVecArr = new double[3];
+            _tVec.get(0, 0, tVecArr);
+            PoseData poseData = ARUtils.ConvertRvecTvecToPoseData(rVecArr, tVecArr);
+            // 閾値以下ならば更新を無視
+            ARUtils.LowpassPoseData(ref _oldPoseData, ref poseData, positionLowPass, rotationLowPass);
+            _oldPoseData = poseData;
+            // 変換行列を作成
+            _transformationM = Matrix4x4.TRS(poseData.pos, poseData.rot, Vector3.one);
+        }
+
+
+        Calib3d.Rodrigues(_rVec, _rotM);
+
+        _transformationM.SetRow(0,
+            new Vector4(
+                (float) _rotM.get(0, 0)[0],
+                (float) _rotM.get(0, 1)[0],
+                (float) _rotM.get(0, 2)[0],
+                (float) _tVec.get(0, 0)[0]));
+        _transformationM.SetRow(1,
+            new Vector4(
+                (float) _rotM.get(1, 0)[0],
+                (float) _rotM.get(1, 1)[0],
+                (float) _rotM.get(1, 2)[0],
+                (float) _tVec.get(1, 0)[0]));
+        _transformationM.SetRow(2,
+            new Vector4(
+                (float) _rotM.get(2, 0)[0],
+                (float) _rotM.get(2, 1)[0],
+                (float) _rotM.get(2, 2)[0],
+                (float) _tVec.get(2, 0)[0]));
+        _transformationM.SetRow(3,
+            new Vector4(
+                0,
+                0,
+                0,
+                1));
+
+        _ARM = _invertYM * _transformationM * _invertYM;
+        _ARM = _ARM * _invertYM * _invertZM;
+
+        return ExtractRotationFromMatrix(ref _ARM).eulerAngles;
+    }
+    
+    void SetParameter(CubismParameter parameter, float value)
+    {
+        if (parameter != null)
+        {
+            parameter.Value = Mathf.Clamp(value, parameter.MinimumValue, parameter.MaximumValue);
+        }
+    }
+
+
+    /// <summary>
+    /// 変換行列からクォータニオンを導出
+    /// </summary>
+    private static Quaternion ExtractRotationFromMatrix(ref Matrix4x4 matrix)
+    {
+        Vector3 forward;
+        forward.x = matrix.m02;
+        forward.y = matrix.m12;
+        forward.z = matrix.m22;
+
+        Vector3 upwards;
+        upwards.x = matrix.m01;
+        upwards.y = matrix.m11;
+        upwards.z = matrix.m21;
+
+        return Quaternion.LookRotation(forward, upwards);
+    }
+}
